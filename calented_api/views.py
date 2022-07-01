e89bf537790c313faada40beae25cd960e935099
LaffSteven <steve.laff@gmail.com>
2022-07-01 19:34:41 -0400

    created CalEvent urls

--- calented_api/views.py
+++ calented_api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import CalEventSerializer
+from .models import CalEvent
 # Create your views here.
+
+
+class CalEventList(generics.ListCreateAPIView):
+    queryset = CalEvent.objects.all().order_by('id') # tell django how to retrieve all objects from the DB, order by id ascending
+    serializer_class = CalEventSerializer # tell django what serializer to use
+
+class CalEventDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CalEvent.objects.all().order_by('id')
+    serializer_class = CalEventSerializer
