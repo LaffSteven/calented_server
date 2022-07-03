c3a169f8c907ddc6cd5eeab7a406b7a6e5855d54
LaffSteven <steve.laff@gmail.com>
2022-07-03 17:10:08 -0400

    changed to date and time fields from datetime field

--- calented_api/serializers.py
+++ calented_api/serializers.py
@@ -5,4 +5,4 @@ from .models import CalEvent
 class CalEventSerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
     class Meta:
         model = CalEvent # tell django which model to use
-        fields = ('id','date', 'title', 'description',) # tell django which fields to include
+        fields = ('id','date', 'time', 'title', 'description',) # tell django which fields to include
